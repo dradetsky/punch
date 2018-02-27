efb3b3c9d3d73e5acd89694b08bd972c50acf9b6
Daniel Radetsky <dradetsky@gmail.com>
2018-02-26 22:45:12 -0800

    prep for publish

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
-
 install_requires = (
     # XXX(dmr, 2018-02-26): possibly not minimum version
     'munch>=2.2.0'
 )
 
 setup(
-    name='punch',
+    name='punch-it',
     version='0.0.0',
     description='munch not fast enough? just punch.it',
     author='dmr',
     author_email='dradetsky@gmail.com',
+    url='https://github.com/dradetsky/punch',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=install_requires,
